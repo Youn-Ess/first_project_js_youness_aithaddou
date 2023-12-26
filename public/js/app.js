f530b591522223bc482e2ec2e3468873ae134e10
youness <younessaithadou9@gmail.com>
2023-12-26 14:45:20 +0100

    testing the password

--- public/js/app.js
+++ public/js/app.js
@@ -64,24 +64,38 @@ let specialAtmark = /[@]/
 // console.log(email);
 
 //^ promt email with test
-let Age = prompt(`enter your age`).trim();
-while (Age.length == 0 || !specialNumbers.test(Age) || (Age > 100 || Age < 18)) {
-    if (Age.length == 0) {
-        alert(`you can't leave your age empty`);
-    } else if (!specialNumbers.test(Age)) {
-        alert(`you can't enter a string please enter a number`)
-    } else if (Age > 100 || Age < 18) {
-        if (Age > 100) {
-            alert(`rak mati`)
-            break;
-        }else if(Age < 18){
-            alert(`rak ba9i sghir`)
-            break;
-        }
+// let Age = prompt(`enter your age`).trim();
+// while (Age.length == 0 || !specialNumbers.test(Age) || (Age > 100 || Age < 18)) {
+//     if (Age.length == 0) {
+//         alert(`you can't leave your age empty`);
+//     } else if (!specialNumbers.test(Age)) {
+//         alert(`you can't enter a string please enter a number`)
+//     } else if (Age > 100 || Age < 18) {
+//         if (Age > 100) {
+//             alert(`rak mati`)
+//             break;
+//         }else if(Age < 18){
+//             alert(`rak ba9i sghir`)
+//             break;
+//         }
+//     }
+//     Age = prompt(`enter your age again`).trim();
+// }
+// console.log(Age)
+
+//* promt email with test
+let Password = prompt(`enter your password`).trim().split(` `).join(``);
+
+while (Password.length < 7 || !specialAtmark.test(Password)) {
+    if (Password.length < 7) {
+        alert(`your password must contain at least 7 characters`);
+        Password = prompt(`enter your password again`).trim().split(` `).join(``);
+    } if (!specialAtmark.test(Password)) {
+        alert(`Require at least one special character`)
+        Password = prompt(`enter your password again`).trim().split(` `).join(``);
     }
-    Age = prompt(`enter your age again`).trim();
 }
 
+console.log(Password)
 
-console.log(Age)
 
