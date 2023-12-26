0088c328b3b9c97043728208533f35df8e442bd2
youness <younessaithadou9@gmail.com>
2023-12-26 14:13:41 +0100

    testing the email

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,68 @@
+let DataBase = [
+    {
+        name: `zakaria`,
+        email: `zakaria@gmail.com`,
+        age: 18,
+        password: `0000`,
+        password_confirmed: `0000`,
+    }
+];
+
 let specialChars = /[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/;
 let specialNumbers = /\d/
+let specialAtmark = /[@]/
 
 
-//todo promt full name with test
-
-let Fullname = prompt(`enter your full name`).trim().split(` `);
-for (let i = 0; i < Fullname.length; i++) {
-    Fullname[i] = Fullname[i][0].toUpperCase() + Fullname[i].slice(1).toLowerCase();
-}
-let FullnameWithOutSpaces = Fullname.join(``);
-let FullnameWithSpaces = Fullname.join(` `);
-
+// //todo promt full name with test
+// let Fullname = prompt(`enter your full name`).trim().split(` `);
+// for (let i = 0; i < Fullname.length; i++) {
+//     Fullname[i] = Fullname[i][0].toUpperCase() + Fullname[i].slice(1).toLowerCase();
+// }
+// let FullnameWithOutSpaces = Fullname.join(``);
+// let FullnameWithSpaces = Fullname.join(` `);
 
 
+// while (FullnameWithOutSpaces.length < 5 || specialChars.test(FullnameWithOutSpaces) || specialNumbers.test(FullnameWithOutSpaces)) {
+//     if (FullnameWithOutSpaces.length < 5) {
+//         alert(`your name must contain at least 5 letters`);
+//     } else if (specialChars.test(FullnameWithOutSpaces)) {
+//         alert(`we can't accept a name with special char`);
+//     }else if(specialNumbers.test(FullnameWithOutSpaces)){
+//         alert(`we can't accept a name with numbers`);
+//     }
+//     Fullname = prompt(`enter your full name`).trim().split(` `);
+//     for (let i = 0; i < Fullname.length; i++) {
+//         Fullname[i] = Fullname[i][0].toUpperCase() + Fullname[i].slice(1).toLowerCase();
+//     }
+//     FullnameWithOutSpaces = Fullname.join(``);
+//     FullnameWithSpaces = Fullname.join(` `);
+// }
+// console.log(FullnameWithSpaces);
 
-while (FullnameWithOutSpaces.length < 5 || specialChars.test(FullnameWithOutSpaces) || specialNumbers.test(FullnameWithOutSpaces)) {
-    if (FullnameWithOutSpaces.length < 5) {
-        alert(`your name must contain at least 5 char`);
-    } else if (specialChars.test(FullnameWithOutSpaces)) {
-        alert(`we can't accept a name with special char`);
-    }else if(specialNumbers.test(FullnameWithOutSpaces)){
-        alert(`we can't accept a name with numbers`);
+
+
+//! promt email with test
+
+let email = prompt(`enter your email`).trim().toLowerCase().split(` `).join(``);
+
+while (!specialAtmark.test(email) || email.length < 10) {
+    if (!specialAtmark.test(email)) {
+        alert(`your email must contain atmark (@)`)
+    } else if (email.length < 10) {
+        alert(`your email must contain at least 10 letters`);
     }
-    Fullname = prompt(`enter your full name`).trim().split(` `);
-    for (let i = 0; i < Fullname.length; i++) {
-        Fullname[i] = Fullname[i][0].toUpperCase() + Fullname[i].slice(1).toLowerCase();
-    }
-    FullnameWithOutSpaces = Fullname.join(``);
-    FullnameWithSpaces = Fullname.join(` `);
+    email = prompt(`enter your email`).trim().toLowerCase().split(` `).join(``);
 }
-console.log(FullnameWithSpaces);
 
+for (let i = 0; i < DataBase.length; i++) {
+    let element = DataBase[i];
+    while (email == element.email) {
+        alert(" the email should be unique");
+        email = prompt(`enter your email`).trim().toLowerCase().split(` `).join(``);
+    }
+}
 
-
+console.log(email);
 
 
 
